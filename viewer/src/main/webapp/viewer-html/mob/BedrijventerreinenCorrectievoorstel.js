3119b27e3616ab9e1b93dda6b128e1db8890cbe2
Meine Toonen <toonen.meine@gmail.com>
2019-01-09 14:05:16 +0100

    cleanup

--- viewer/src/main/webapp/viewer-html/mob/BedrijventerreinenCorrectievoorstel.js
+++ viewer/src/main/webapp/viewer-html/mob/BedrijventerreinenCorrectievoorstel.js
@@ -9,8 +9,8 @@
 
 Ext.define("viewer.components.BedrijventerreinenCorrectievoorstel", {
     extend: "viewer.components.Component",
-    container:null,
-    buttonContainer:null,
+    container: null,
+    buttonContainer: null,
     stores: {},
     config: {
         title: null,
@@ -18,13 +18,13 @@ Ext.define("viewer.components.BedrijventerreinenCorrectievoorstel", {
         tooltip: null,
         label: "",
         details: {
-            minWidth:700,
+            minWidth: 700,
             minHeight: 600,
             useExtLayout: true
         }
     },
     constructor: function (conf) {
-        if (!Ext.isDefined(conf.showLabels)){
+        if (!Ext.isDefined(conf.showLabels)) {
             conf.showLabels = true;
         }
         this.initConfig(conf);
@@ -32,29 +32,18 @@ Ext.define("viewer.components.BedrijventerreinenCorrectievoorstel", {
         this.createStores();
         return this;
     },
-    newCorrection: function(){
-        
-    },
-    
-    loadWindow: function () {
-        var me = this;
-        this.renderButton({
-            handler: function () {
-                me.popup.show();
-            },
-            text: me.config.title,
-            icon: me.config.iconUrl,
-            tooltip: me.config.tooltip,
-            label: me.config.label
-        });
+    newCorrection: function () {
 
+    },
+
+    loadWindow: function () {
         this.container = Ext.create('Ext.window.Window', {
             layout: {
                 type: 'hbox',
                 align: 'stretch'
             },
-                width: 600,
-                height: 600,
+            width: 600,
+            height: 600,
             padding: '5px',
             items: this.createForm()
 
@@ -67,38 +56,38 @@ Ext.define("viewer.components.BedrijventerreinenCorrectievoorstel", {
             title: 'Correctievoorstel',
             items: [
                 {
-                    xtype: 'container',layout:{type:'hbox',pack:"end"},items:[
-                        {xtype:'label',text:'1 juli 2018', itemId:'peildatum'}
+                    xtype: 'container', layout: {type: 'hbox', pack: "end"}, items: [
+                        {xtype: 'label', text: '1 juli 2018', itemId: 'peildatum'}
                     ]
                 },
                 {
-                    xtype: 'container', layout: {type: 'hbox'}, defaults: {margin: '5px',padding: '5px'}, items: [
+                    xtype: 'container', layout: {type: 'hbox'}, defaults: {margin: '5px', padding: '5px'}, items: [
                         {xtype: 'textfield', value: 'Almelo', editable: false, itemId: 'gemeente'},
                         {xtype: 'textfield', value: 'Turfkade', flex: 1, editable: false, itemId: 'plan'}
                     ]
                 },
                 {
-                    xtype: 'container', layout: {type: 'hbox'}, defaults: {padding: '5px'}, items:[
-                        {xtype: 'combobox', flex: 2, labelAlign:'top', name: 'classificatie', fieldLabel: "Voorgestelde classificatie", displayField:"CLASSIFICATIE", valueField: "CLS_ID", store: this.stores.classificaties},
-                        {xtype:'filefield',flex: 1, labelAlign:'top', fieldLabel: "Upload", buttonOnly:true, buttonText:'Upload shp-zip, pdf, ...', itemId:'shp'}
+                    xtype: 'container', layout: {type: 'hbox'}, defaults: {padding: '5px'}, items: [
+                        {xtype: 'combobox', flex: 2, labelAlign: 'top', name: 'classificatie', fieldLabel: "Voorgestelde classificatie", displayField: "CLASSIFICATIE", valueField: "CLS_ID", store: this.stores.classificaties},
+                        {xtype: 'filefield', flex: 1, labelAlign: 'top', fieldLabel: "Upload", buttonOnly: true, buttonText: 'Upload shp-zip, pdf, ...', itemId: 'shp'}
                     ]
                 },
                 {
-                    xtype:"textarea",fieldLabel:"Toelichting", padding: '5px',labelAlign:'top',itemId:"toelichting", anchor:'100%'
+                    xtype: "textarea", fieldLabel: "Toelichting", padding: '5px', labelAlign: 'top', itemId: "toelichting", anchor: '100%'
                 },
                 {
-                    xtype: 'container', layout: {type: 'hbox'}, items:[
-                        {flex: 1, xtype: 'combobox', labelAlign:'top', name: 'status', margin: '5px',padding: '5px', fieldLabel: "Status", value: "Opgeslagen", store: this.stores.status},
-                        {flex: 2, xtype:'container', layout:{type: 'hbox', pack: 'end'},defaults: {margin: '5px',padding: '5px'}, items:[
-                            {xtype:'textfield', labelAlign:'top', fieldLabel: 'Laatste wijziging gemeente', value:'10-10-2018', itemId:'datumLaatstGewijzigdGemeente'},
-                            {xtype:'textfield', labelAlign:'top', fieldLabel: "Naam", value:'PJansen', itemId:'naamLaatstGewijzigdGemeente'}]
+                    xtype: 'container', layout: {type: 'hbox'}, items: [
+                        {flex: 1, xtype: 'combobox', labelAlign: 'top', name: 'status', margin: '5px', padding: '5px', fieldLabel: "Status", value: "Opgeslagen", store: this.stores.status},
+                        {flex: 2, xtype: 'container', layout: {type: 'hbox', pack: 'end'}, defaults: {margin: '5px', padding: '5px'}, items: [
+                                {xtype: 'textfield', labelAlign: 'top', fieldLabel: 'Laatste wijziging gemeente', value: '10-10-2018', itemId: 'datumLaatstGewijzigdGemeente'},
+                                {xtype: 'textfield', labelAlign: 'top', fieldLabel: "Naam", value: 'PJansen', itemId: 'naamLaatstGewijzigdGemeente'}]
                         }
                     ]
                 },
                 {
-                    xtype: 'container', layout: {type: 'hbox', pack:'end'}, defaults: {margin: '5px',padding: '5px'}, items:[
-                        {xtype:'textfield', labelAlign:'top', fieldLabel: 'Laatste wijziging provincie', value:'11-10-2018', itemId:'datumLaatstGewijzigdProvincie'},
-                        {xtype:'textfield', labelAlign:'top', fieldLabel: "Naam", value:'DvabderVeen', itemId:'naamLaatstGewijzigdProvincie'}
+                    xtype: 'container', layout: {type: 'hbox', pack: 'end'}, defaults: {margin: '5px', padding: '5px'}, items: [
+                        {xtype: 'textfield', labelAlign: 'top', fieldLabel: 'Laatste wijziging provincie', value: '11-10-2018', itemId: 'datumLaatstGewijzigdProvincie'},
+                        {xtype: 'textfield', labelAlign: 'top', fieldLabel: "Naam", value: 'DvabderVeen', itemId: 'naamLaatstGewijzigdProvincie'}
                     ]
                 }
             ],
@@ -111,19 +100,19 @@ Ext.define("viewer.components.BedrijventerreinenCorrectievoorstel", {
                         this.remove();
                     }
                 },
-                {xtype: 'button', text: 'Annuleren'}               
+                {xtype: 'button', text: 'Annuleren'}
             ]
         });
         return this.form;
     },
-    createButtons: function(){
-       if(this.buttonContainer === null) {
+    createButtons: function () {
+        if (this.buttonContainer === null) {
             this.buttonContainer = Ext.create('Ext.container.Container', {
-                renderTo:   Ext.get(this.config.viewerController.getMapId()),
+                renderTo: Ext.get(this.config.viewerController.getMapId()),
                 floating: true,
                 border: false,
                 shadow: false,
-                defaults:{
+                defaults: {
                     margin: '5px'
                 },
                 style: {
@@ -131,45 +120,43 @@ Ext.define("viewer.components.BedrijventerreinenCorrectievoorstel", {
                 }
             });
         }
-        if(this.cvbutton) {
+        if (this.cvbutton) {
             this.cvbutton.destroy();
             this.vubutton.destroy();
             this.indienbutton.destroy();
         }
-        var me = this;
-       
         this.cvbutton = Ext.create('Ext.Button', {
             text: 'Correctievoorstel',
             handler: this.newCorrection
         });
-       
+
         this.vubutton = Ext.create('Ext.Button', {
             text: 'Verwachte uitgifte',
             handler: function () {
                 alert('You clicked the verwachte uitgifte button!');
             }
         });
-       
+
         this.indienbutton = Ext.create('Ext.Button', {
             text: 'Indienen',
             handler: function () {
                 alert('You clicked the indienen button!');
             }
         });
-        this.buttonContainer.add(this.cvbutton); 
-        this.buttonContainer.add(this.vubutton); 
-        this.buttonContainer.add(this.indienbutton); 
+        this.buttonContainer.add(this.cvbutton);
+        this.buttonContainer.add(this.vubutton);
+        this.buttonContainer.add(this.indienbutton);
         this.alignButtons();
     },
-    alignButtons: function() {
-        if(!this.buttonContainer) {
+    alignButtons: function () {
+        if (!this.buttonContainer) {
             return;
         }
         var pos = [Number(-20), Number(5)];
         var align = 'tr';
         var mapContainer = Ext.get(this.config.viewerController.getMapId());
-        this.buttonContainer.alignTo(mapContainer, [align, align].join('-'),pos);
-        this.config.viewerController.anchorTo(this.buttonContainer, mapContainer, [align, align].join('-'),pos);
+        this.buttonContainer.alignTo(mapContainer, [align, align].join('-'), pos);
+        this.config.viewerController.anchorTo(this.buttonContainer, mapContainer, [align, align].join('-'), pos);
     },
     createStores: function () {
         Ext.define('User', {
